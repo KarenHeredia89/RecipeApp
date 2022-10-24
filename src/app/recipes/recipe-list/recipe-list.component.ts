896d12a9ba883361dcd858e018848542c5175400
Karen Heredia <karenherediadg@gmail.com>
2022-10-23 21:03:30 -0500

    Data Binding Components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();    
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a simple test', 'https://dam.cocinafacil.com.mx/wp-content/uploads/2019/10/dulce-calabaza.jpg'),
     new Recipe('A Test Recipe', 'This is a simple test', 'https://dam.cocinafacil.com.mx/wp-content/uploads/2019/10/dulce-calabaza.jpg')
@@ -17,6 +18,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected( recipe: Recipe ) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
